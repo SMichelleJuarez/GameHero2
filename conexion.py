694f7491385317b8cf7e7497d4cb03d4fe91235c
Sofia Michelle <91221662+SMichelleJuarez@users.noreply.github.com>
2024-07-17 11:44:15 -0300

    Add files via upload
    Subida de archivos Python

---
+++ conexion.py
@@ -0,0 +1,7 @@
+host='SofiaMJuarez.mysql.pythonanywhere-services.com',
+user='SofiaMJuarez',
+password='S1234ofia',
+database='SofiaMJuarez$GameHero'
+username= "SofiaMJuarez"
+api_token = "4a41073ead38ffeedb4de2cdfbf16fdda9654c56"
+domain_name = "SofiaMJuarez.pythonanywhere.com"
